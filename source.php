099169392f7dae7de4c0298e17dc0b3db3f92026
Claudius Coenen <amenthes@Kepler.local>
2015-09-05 21:36:08 +0200

    server sent event demo

---
+++ source.php
@@ -0,0 +1,26 @@
+<?php
+error_log('script starting');
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+echo "retry: 3000\n";
+
+$seed = rand();
+$count = 0;
+
+
+function sendMsg($msg) {
+	global $count, $seed;
+	error_log('messaege sent: ' . $msg);
+	echo "event: testeventcc\n";
+	echo "data: $msg $count $seed\n\n";
+	ob_flush();
+	flush();
+}
+
+
+for ($i = 0; $i < 100; $i++) {
+	$count++;
+	sendMsg('server time: ' . date("H:i:s"));
+	usleep(10000); // 10ms per message -> 100msg/s
+}
